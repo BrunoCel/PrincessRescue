754911beab30d1ee7e396eaa61003447237c23c0
Bruno Celestino <brunoocelestino@gmail.com>
2025-10-08 09:29:58 -0400

    added attack animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rigidbody2D;
+     private Rigidbody2D rigidbody2D;
+     private Animator animator;
      private float inputX;
+     
      [SerializeField] float moveSpeed = 5f;
      [SerializeField] float jumpForce = 10f;
+     
+     private bool facingRight = true;
+     private bool isGrounded;
+     [SerializeField] float groundDistance = 1.4f;
+     [SerializeField] LayerMask whatIsGround;
 
     private void Awake()
     {
+        animator = GetComponentInChildren<Animator>();
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
     
@@ -22,9 +30,26 @@ public class Player : MonoBehaviour
     
     void Update()
     {
-        
+        HandleCollisions();
         HandleInput();
         HandleMovement();
+        HandleAnimations();
+        HandleFlip();
+        
+
+    }
+    void HandleMovement()
+    {
+        rigidbody2D.velocity = new Vector2(inputX * moveSpeed, rigidbody2D.velocity.y);
+    }
+
+    private void HandleAnimations()
+    {
+        
+        
+            animator.SetBool("isGrounded",isGrounded);
+            animator.SetFloat("yVelocity" , rigidbody2D.velocity.y);
+            animator.SetFloat("xVelocity" , rigidbody2D.velocity.x);
         
     }
 
@@ -34,17 +59,51 @@ public class Player : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Jump();
+            TryToJump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            TryToAttack();
         }
     }
 
-    void Jump()
+    private void TryToAttack()
     {
-        rigidbody2D.AddForce(Vector2.up * jumpForce,ForceMode2D.Impulse);
+        animator.SetTrigger("attack");
     }
 
-    void HandleMovement()
+    void HandleFlip()
     {
-        rigidbody2D.velocity = new Vector2(inputX * moveSpeed, rigidbody2D.velocity.y);
+        if (rigidbody2D.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }else if (rigidbody2D.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void HandleCollisions()
+    {
+        isGrounded = Physics2D.Raycast(transform.position,Vector2.down, groundDistance,whatIsGround);
+    }
+
+    void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+    void TryToJump()
+    {
+        if (isGrounded)
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position,transform.position + new Vector3(0,groundDistance));
     }
 }
